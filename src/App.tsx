ac59f68122b3e482196dff3c552fe1d500864528
Himanshu Negi <himanshu.21negi@gmail.com>
2023-03-08 23:41:13 +0530

    Word list is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import words from "./wordList.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Props = {};
+
+const App = (props: Props) => {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const nextWord = (): void => {
+    setWordToGuess(words[Math.floor(Math.random() * words.length)]);
+  };
+
+  console.log(wordToGuess);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <button onClick={nextWord}>Change word</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
